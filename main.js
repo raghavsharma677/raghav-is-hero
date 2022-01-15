1a38cce7b517e108a9b029f8efca83e1e99c2c09
raghavsharma677 <81424060+raghavsharma677@users.noreply.github.com>
2022-01-15 18:59:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting object";
+}
+
+img = "";
+status = "";
+objects= [];
+
+function draw() {
+    Image = (img, 0, 0, 640, 420);
+     
+    if (status !="")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status Object Detecting";
+
+            fill("#FF0000");
+            percent = floor(objects [i].confidendence * 100);
+            text(objects [i].label +" "+ percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function preload() {
+    img = loadImage('dog_cat.jpg')
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
